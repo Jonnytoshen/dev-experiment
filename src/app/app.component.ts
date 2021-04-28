5bdfcfc49bc4e55d66115677ad3dbbb2b874f4df
Jonny <jonnytoshen@gmail.com>
2021-04-28 14:40:10 +0800

    chore(AppComponent): remove console.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { filter, map, pluck, tap } from 'rxjs/operators';
+import { filter, map, pluck } from 'rxjs/operators';
 
 @Component({
   selector: 'dev-root',
@@ -20,7 +20,6 @@ export class AppComponent implements OnInit {
     this.backVisible$ = this.router.events
       .pipe(
         filter(event => event instanceof NavigationEnd),
-        tap(event => console.log(event)),
         pluck('url'),
         map(url => url !== '/' && url !== '/table-content')
       );
